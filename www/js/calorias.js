37bdbccd0afca70d115b1090ba2ea6b14ff02ba6
Pedro Github <testandoduca@gmail.com>
2020-05-19 19:31:19 -0300

    Finalizado calculo de caloria e melhorias na interface

--- www/js/calorias.js
+++ www/js/calorias.js
@@ -1,32 +1,22 @@
-var app = angular.module("myApp2", []);
-
-app.controller("myCtrl2", function ($scope) {
+angular.module("myApp", [])
+.controller("myCtrl2", function ($scope) {
   
-  var caloriaGasta = $scope.caloriaGasta;
   $scope.calculoCal = function () {
-    var caloriaGasta = 0;
-
+    var caloriaPorExercicio = {
+      musculacao: 8.9,
+      esteira: 16,
+      bicicleta: 4.9
+    }
+    var caloriaGasta;
     if ($scope.select == "treinoA") {
-      caloriaGasta = $scope.peso * ($scope.tempo * 8.9);
+      caloriaGasta = $scope.pesoCal * ($scope.tempo * caloriaPorExercicio.musculacao);
     } else if ($scope.select == "treinoB") {
-      caloriaGasta = $scope.peso * ($scope.tempo - 30) * 8.9 + 30 * 16;
+      caloriaGasta = $scope.pesoCal * ($scope.tempo - 30) * caloriaPorExercicio.musculacao + (30 * caloriaPorExercicio.esteira); 
     } else if ($scope.select == "treinoC") {
-      caloriaGasta = $scope.peso * ($scope.tempo - 20) * 8.9 + 20 * 4.9;
+      caloriaGasta = $scope.pesoCal * ($scope.tempo - 20) * treino.caloria + (20 * caloriaPorExercicio.bicicleta);
     }
 
     $scope.caloriaGasta = caloriaGasta;
     };
     
 });
-
-/*
-
-tempo * peso * atividade
-
-treino A peso x (60 min * 8.9)
-
-treino B peso X (tempo de treino - 30 minutos) *8.9 + ( 30 minutos * 16)
-
-treino c peso x ( tempo de treino - 20 minutos) *8.9 + ( 20 minutos * 4.9)
-
-*/
