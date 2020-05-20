28df8ca3ad247a729197388c7f122d894a937ac1
Pedro Github <testandoduca@gmail.com>
2020-05-19 21:50:30 -0300

    Adicionado localStorage e preenchimento do relatorio

--- www/js/imc.js
+++ www/js/imc.js
@@ -37,9 +37,45 @@ angular.module("myApp", [])
       } else if ($scope.select == "treinoB") {
         caloriaGasta = $scope.pesoCal * ($scope.tempo - 30) * caloriaPorExercicio.musculacao + (30 * caloriaPorExercicio.esteira); 
       } else if ($scope.select == "treinoC") {
-        caloriaGasta = $scope.pesoCal * ($scope.tempo - 20) * treino.caloria + (20 * caloriaPorExercicio.bicicleta);
+        caloriaGasta = $scope.pesoCal * ($scope.tempo - 20) * caloriaPorExercicio.musculacao + (20 * caloriaPorExercicio.bicicleta);
       }
 
       $scope.caloriaGasta = caloriaGasta;
+      var data = getDateNow();
+      var dadosRelatorios = {
+        nome: $scope.nome,
+        data: data,
+        caloriasGastas: caloriaGasta,
+        treinoUtilizado: $scope.select,
+        tempoTotal: $scope.tempo
       };
+
+      localStorage.setItem('dadosRelatorios', JSON.stringify(dadosRelatorios));
+
+    };
+
+    /**
+     * Relatorio
+     */
+    $scope.relatorio = function(){
+      var local = JSON.parse(localStorage.getItem('dadosRelatorios'));
+      
+      console.log(JSON.stringify(local.data));
+      $scope.dataTable = JSON.stringify(local.data);
+      $scope.treinoTable = JSON.stringify(local.treinoUtilizado);
+      $scope.caloriaGastaTable = JSON.stringify(local.caloriasGastas);
+      $scope.tempoTotalTable = JSON.stringify(local.tempoTotal);
+      
+    };
 });
+
+function getDateNow(){
+  var currentdate = new Date(); 
+  var datetime = currentdate.getDate() + "/"
+            + (currentdate.getMonth()+1)  + "/" 
+            + currentdate.getFullYear() + " "  
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + ":" 
+            + currentdate.getSeconds();
+  return datetime;
+}
