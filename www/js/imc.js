0205f296c114125556cbe867c185c3a3a7aa22dc
Pedro Github <testandoduca@gmail.com>
2020-05-31 15:51:36 -0300

    Fix valor imc sempre abaixo do ideal e quebrou geração de relatório

--- www/js/imc.js
+++ www/js/imc.js
@@ -4,26 +4,28 @@ angular.module("myApp", [])
   /**
    * Calculo IMC
    */
-  var message = 'Preencha os dados';
+  $scope.msg = 'Preencha os dados'
   $scope.calculoIMC = function () {
-    var IMC = $scope.peso / ($scope.altura * $scope.altura);
+    var message;
+    var IMC = parseFloat($scope.peso / ($scope.altura * $scope.altura));
+    dadosRelatorios.valorImc = IMC;
     if (IMC < 18.5) {
-      message = "Seu Imc é " + IMC + ", Abaixo do ideal";
+      message = "Seu Imc é < 18.5, Abaixo do ideal";
     } else if (IMC >= 18.5 && IMC <= 24.5) {
-      message = "Seu Imc " + IMC + ", está no ideal";
+      message = "Seu Imc 18.5 a 24.5, está no ideal";
     } else if (IMC >= 25 && IMC <= 29.9) {
-      message = "Seu Imc é " + IMC + ", você está com sobrepeso";
+      message = "Seu Imc é 25 a 29.9, você está com sobrepeso";
     } else if (IMC >= 30 && IMC <= 39.9) {
-      message = "Seu Imc é " + IMC + ", você está com obesidade";
+      message = "Seu Imc é 30 a 39.9, você está com obesidade";
     } else if (IMC >= 40) {
-      message = "Seu Imc é " + IMC + ", você está com obesidade grave";
+      message = "Seu Imc é < 40, você está com obesidade grave";
     }
     $scope.msg = message;
   }
-  $scope.msg = message;
   /**
    * Calculo calorias
    */
+  $scope.caloriagasta = 'Preencha os dados'
   $scope.calculoCaloria = function () {
     var caloriaPorExercicio = {
       musculacao: 8.9,
@@ -38,50 +40,49 @@ angular.module("myApp", [])
     } else if ($scope.select == "treino C") {
       caloriaGasta = $scope.pesoCal * ($scope.tempo - 20) * caloriaPorExercicio.musculacao + (20 * caloriaPorExercicio.bicicleta);
     }
-    $scope.caloriaGasta = caloriaGasta;
+    $scope.caloriagasta = caloriaGasta;
     
-    var data = getDateNow();
     var dadosRelatorios = {
-      nome: $scope.nome,
-      data: data,
-      caloriasGastas: caloriaGasta,
-      treinoUtilizado: $scope.select,
-      tempoTotal: $scope.tempo
-    };
-    
-    setLocalStoraged(dadosRelatorios); 
 
     };
+    dadosRelatorios.nome = $scope.nome;
+    dadosRelatorios.data = getDateNow();
+    dadosRelatorios.caloriasGastas = caloriaGasta;
+    dadosRelatorios.treinoUtilizado = $scope.select;
+    dadosRelatorios.tempoTotal = $scope.tempo;
+    var novo = getIndex(); 
+    localStorage.setItem(novo, JSON.stringify(dadosRelatorios));
+  };
 
-    /**
-     * Relatorio
-     */
-    $scope.relatorio = function(){
-      var index = JSON.parse(localStorage.getItem('index'));
-      for(var i = 0; i < index.length; i++){
-        if(index != null){
-          var local = JSON.parse(localStorage.getItem(index[i]));
-          dataTablet.innerHTML = JSON.stringify(local.data); 
-          treinoTablet.innerHTML = JSON.stringify(local.treinoUtilizado); 
-          caloriaGastaTablet.innerHTML = JSON.stringify(local.caloriasGastas); 
-          tempoTotalTablet.innerHTML = JSON.stringify(local.tempoTotal); 
-        }
+  /**
+   * Relatorio
+   */
+  $scope.relatorio = function(){
+    var index = JSON.parse(localStorage.getItem('index'));
+    for(var i = 0; i < index.length; i++){
+      if(index != null){
+        var local = JSON.parse(localStorage.getItem(index[i]));
+        nomeTablet.innerHTML = JSON.stringify(local.nome);
+        dataTablet.innerHTML = JSON.stringify(local.data); 
+        treinoTablet.innerHTML = JSON.stringify(local.treinoUtilizado); 
+        caloriaGastaTablet.innerHTML = JSON.stringify(local.caloriasGastas) + ' kcal'; 
+        tempoTotalTablet.innerHTML = JSON.stringify(local.tempoTotal) + ' minutos';
       }
-    };
+    }
+  };
 });
 
-function setLocalStoraged(dadosRelatorios){
-  var localIndex = JSON.parse(localStorage.getItem('index'));
+function getIndex(){
+  var index = JSON.parse(localStorage.getItem('index'));
   var novo;
-  if(localIndex == null) {
+  if(index == null) {
     localStorage.setItem('index', '[0]');
     novo = 0;
   } else {
-    novo = localIndex.length;
-    localIndex.push(novo);
-    localStorage.setItem('index', JSON.stringify(localIndex));
+    novo = index.length;
+    index.push(novo);
+    localStorage.setItem('index', JSON.stringify(index));
   }
-  localStorage.setItem(novo, JSON.stringify(dadosRelatorios));
   return novo;
 }
 
